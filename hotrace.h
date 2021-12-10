7b185a28de73a37b1a8bdbaf16bf365ad85bb189
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-11 00:40:29 +0100

    Added attributes to btree functions, added btree_insert prototype

--- hotrace.h
+++ hotrace.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 21:54:14 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/11 00:25:28 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/11 00:39:26 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,27 @@ void			*ft_memmove(void *dst, void const *src, unsigned int n);
 
 void			*ft_memset(void *dst, int c, unsigned int n);
 
-char			*ft_strdup(char const *s);
+char			*ft_strdup(char const *s)
+				__attribute__((nonnull(1), malloc));
 
-unsigned int	ft_strlen(char const *s);
+unsigned int	ft_strlen(char const *s)
+				__attribute__((nonnull(1)));
 
+/* get the hash for a given NUL-terminated string */
 t_hash			hash_str(char const *s);
 
 /* create a single node with left and right to nil */
-t_btree			*btree_create(char const *key, char *value);
+t_btree			*btree_create(char const *key, char *value)
+				__attribute__((nonnull(1), malloc));
 
 /* recursively destroy a tree */
 void			btree_destroy(t_btree *root);
 
+/* search for a specific hash in a tree */
 t_btree			*btree_search(t_btree *root, t_hash hash);
+
+/* insert a new node in a tree */
+t_btree			*btree_insert(t_btree **root, t_btree *node)
+				__attribute__((nonnull(1, 2)));
 
 #endif
