996534567480bdb46ab18853289854b6ba23a66f
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-11 01:54:53 +0100

    Removed declaration and assignation on the same line

--- src/btree/btree_create.c
+++ src/btree/btree_create.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 23:25:20 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/11 00:15:18 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/11 01:09:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 t_btree	*btree_create(char const *key, char *value)
 {
-	t_btree	*tree = malloc(sizeof (*tree));
+	t_btree	*tree;
 
+	tree = malloc(sizeof (*tree));
 	if (tree)
 	{
 		tree->key = hash_str(key);
