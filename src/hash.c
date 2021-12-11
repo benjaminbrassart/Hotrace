5647b8fd6921e2b986a49cecc4836ddac414decc
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-11 02:03:12 +0100

    Renamed ft_hash.c to hash.c

--- src/hash.c
+++ src/hash.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_hash.c                                          :+:      :+:    :+:   */
+/*   hash.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cloew <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 22:38:26 by cloew             #+#    #+#             */
-/*   Updated: 2021/12/10 23:03:54 by cloew            ###   ########.fr       */
+/*   Updated: 2021/12/11 02:03:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
-static unsigned int	rol(unsigned int r, int k)
+#include "hotrace.h"
+
+static t_hash	rol(t_hash r, int k)
 {
 	return ((r << k) | (r >> (32 - k)));
 }
 
-unsigned int	hash(char const *input)
+t_hash	hash(char const *input)
 {
-	unsigned int	result;
+	t_hash	result;
 
 	result = 0x55555555;
 	while (*input)
@@ -28,12 +29,3 @@ unsigned int	hash(char const *input)
 	}
 	return (result);
 }
-/*
-int main(int argc, char **argv)
-{
-    unsigned int        i;
-
-    i = hash(argv[1]);
-    printf("%u\n", i);
-}
-*/
