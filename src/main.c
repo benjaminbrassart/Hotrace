85c584ff4e05f3be00d112bc2de469c3d343e888
Carmen LOEW <cloew@e2r3p18.clusters.42paris.fr>
2021-12-11 01:27:43 +0100

    going to get some sleep

--- src/main.c
+++ src/main.c
@@ -6,11 +6,37 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 21:07:05 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/10 21:09:41 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/11 01:26:54 by cloew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(void)
 {
+	int	gnl;
+	char	*line;
+	int	had;
+
+	gnl = 1;
+	while (gnl)
+	{
+		gnl = get_next_line(0, &line);
+		had = hash(*line);//header
+		gnl = get_next_line(0, &line);
+		//new_node(had, *line)
+		gnl = get_next_line(0, &line);
+		if (!*line)
+		{
+			gnl = get_next_line(0, &line);
+			while (gnl)
+			{
+				hash(*line);
+				//find me
+				gnl = get_next_line(0, &line);
+			}
+			brack
+		}
+
+	}
+
 	return (0);
 }
