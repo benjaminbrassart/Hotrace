6e58713555684f66fa8e71197021dae24cca63c9
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-11 11:08:52 +0100

    Improved get_next_line just a teeny tiny bit

--- src/ft/get_next_line.c
+++ src/ft/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 21:53:14 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/10 22:40:56 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/11 11:07:42 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static void	gnl_join(char **line, char *buffer, size_t n)
+static char	*gnl_realloc(char *line, char *buf, int line_len, int i)
 {
-	size_t	i;
-	char	*s;
+	char	*new_line;
 
-	i = 0;
-	while (*line && (*line)[i])
-		++i;
-	s = malloc(i + n + 1);
-	if (s)
+	new_line = malloc(sizeof (*new_line) * (line_len + i + 1));
+	if (new_line)
 	{
-		ft_memmove(s, *line, i);
-		ft_memmove(s + i, buffer, n);
-		s[i + n] = 0;
-		free(*line);
-		*line = s;
+		ft_memmove(new_line, line, line_len);
+		ft_memmove(new_line + line_len, buf, i);
+		new_line[line_len + i] = 0;
+		free(line);
 	}
+	return (new_line);
 }
 
-static int	gnl_copy(char **line, char *buffer, char **rest, int release)
+static int	gnl_join(char **line, char buf[BUFFER_SIZE], int *buf_len)
 {
-	int	i;
+	int		i;
+	int		line_len;
+	char	*new_line;
+	int		on_newline;
 
-	if (!line)
-		return (0);
+	on_newline = 0;
+	line_len = 0;
+	if (*line)
+		while ((*line)[line_len])
+			++line_len;
 	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
+	while (i < *buf_len && buf[i] != '\n')
 		++i;
-	gnl_join(line, buffer, i);
-	free(*rest);
-	if (buffer[i])
-		*rest = ft_strdup(buffer + i + 1);
-	else
-		*rest = NULL;
-	i = !!buffer[i];
-	if (release)
-		free(buffer);
-	return (i);
+	new_line = gnl_realloc(*line, buf, line_len, i);
+	if (!new_line)
+		return (-1);
+	*line = new_line;
+	if (buf[i] == '\n')
+	{
+		on_newline = 1;
+		++i;
+	}
+	ft_memmove(buf, buf + i, *buf_len - i);
+	*buf_len -= i;
+	return (on_newline);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	static char	*rest = NULL;
-	char		buffer[BUFFER_SIZE + 1];
-	int			bytes;
+	static char	buf[BUFFER_SIZE];
+	static int	buf_len = 0;
+	int			status;
 
-	if (!line)
-		return (-1);
-	bytes = 1;
 	*line = NULL;
-	if (rest)
-		if (gnl_copy(line, ft_strdup(rest), &rest, 1))
-			return (1);
-	while (bytes > 0)
+	while (1)
 	{
-		bytes = read(fd, ft_memset(buffer, 0, BUFFER_SIZE + 1), BUFFER_SIZE);
-		if (bytes >= 0 && gnl_copy(line, buffer, &rest, 0))
-			return (1);
+		if (buf_len > 0)
+		{
+			status = gnl_join(line, buf, &buf_len);
+			if (status)
+				return (status);
+		}
+		buf_len = read(fd, buf, BUFFER_SIZE);
+		if (buf_len <= 0)
+			break ;
 	}
-	return (bytes);
+	return (buf_len);
 }
