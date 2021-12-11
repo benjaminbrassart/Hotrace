39379adc42e9b9bd6119d6efb72e3522d76f5f07
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-11 13:29:51 +0100

    It work! no leaks and all

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 21:07:05 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/11 12:53:45 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/11 13:28:14 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hotrace.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 static void	print_error(char const *keyword)
@@ -24,45 +25,62 @@ static void	print_error(char const *keyword)
 	write(1, NOT_FOUND_SUFFIX, sizeof (NOT_FOUND_SUFFIX) - 1);
 }
 
+static int	parse_init(t_btree **root)
+{
+	char	*line;
+	t_hash	key;
+	t_btree	*new;
+
+	while (1)
+	{
+		if (get_next_line(0, &line) <= 0)
+			return (0);
+		if (!*line)
+			break ;
+		key = hash(line);
+		free(line);
+		if (get_next_line(0, &line) <= 0)
+			return (0);
+		new = btree_create(key, line);
+		if (!new)
+		{
+			free(line);
+			return (0);
+		}
+		btree_insert(root, new);
+	}
+	free(line);
+	return (1);
+}
+
+static void	exec_get_keys(t_btree **root)
+{
+	t_hash	key;
+	char	*line;
+	t_btree	*search;
+
+	while (1)
+	{
+		if (get_next_line(0, &line) <= 0)
+			break ;
+		key = hash(line);
+		search = btree_search(*root, key);
+		if (search)
+			ft_putstr(search->value);
+		else
+			print_error(line);
+		ft_putstr("\n");
+		free(line);
+	}
+}
+
 int	main(void)
 {
-	int		gnl;
-	char		*line;
-	t_hash		key;
-	t_btree		*new;
 	t_btree		*root;
 
-	gnl = 1;
 	root = NULL;
-	while (gnl)
-	{
-		gnl = get_next_line(0, &line);
-		if (!*line)
-		{
-			gnl = get_next_line(0, &line);
-			while (gnl)
-			{
-				key = hash(line);
-				new = btree_search(root, key);
-				if (!new)
-				{
-					ft_putstr(line);
-				}
-				else
-					ft_putstr(new->value);
-				ft_putchar('\n');
-				gnl = get_next_line(0, &line);
-			}
-			break ;
-		}
-	}
-		key = hash(line);
-		gnl = get_next_line(0, &line);
-		new = btree_create(key, *line);
-		if (!new)
-			break ;
-		btree_insert(&root, new);
-	}
+	if (parse_init(&root))
+		exec_get_keys(&root);
 	btree_destroy(root);
 	return (0);
 }
