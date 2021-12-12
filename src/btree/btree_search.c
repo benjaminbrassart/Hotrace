ef90b2b36cc45ac5cc96139c618bca5d2054714f
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-12 21:27:59 +0100

    Switch to an iterative search for btree_search

--- src/btree/btree_search.c
+++ src/btree/btree_search.c
@@ -6,18 +6,20 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 00:21:48 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/12/11 00:24:13 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/12 14:40:05 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hotrace.h"
-#include <stdlib.h>
 
 t_btree	*btree_search(t_btree *root, t_hash hash)
 {
-	if (!root || root->key == hash)
-		return (root);
-	if (hash > root->key)
-		return (btree_search(root->right, hash));
-	return (btree_search(root->left, hash));
+	while (root && root->key != hash)
+	{
+		if (hash > root->key)
+			root = root->right;
+		else
+			root = root->left;
+	}
+	return (root);
 }
